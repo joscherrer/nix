9acac16372493bf0ed1d392f62416d37c4252961
Jonathan SCHERRER <jonathan.s.scherrer@gmail.com>
2023-12-11 00:13:06 +0100

    fix zsh bracketed paste magic lag

--- dotfiles/common/.config/zsh/functions.zsh
+++ dotfiles/common/.config/zsh/functions.zsh
@@ -95,3 +95,16 @@ zmodload zsh/langinfo
 autoload -Uz is-at-least bracketed-paste-magic url-quote-magic
 zle -N bracketed-paste bracketed-paste-magic
 zle -N self-insert url-quote-magic
+
+# This speeds up pasting w/ autosuggest
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
