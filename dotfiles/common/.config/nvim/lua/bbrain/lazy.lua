9fffa1d9f7a93d8838aed74b94be4264739c10f5
Jonathan SCHERRER <jonathan.s.scherrer@gmail.com>
2024-05-05 19:59:58 +0200

    update

--- dotfiles/common/.config/nvim/lua/bbrain/lazy.lua
+++ dotfiles/common/.config/nvim/lua/bbrain/lazy.lua
@@ -48,56 +48,7 @@ local plugins = {
             { 'hrsh7th/cmp-path' },
         },
         config = function()
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
-
-            local cmp = require('cmp')
-
-            cmp.setup({
-                preselect = 'item',
-                completion = {
-                    completeopt = 'menu,menuone,noinsert',
-                },
-                formatting = lsp_zero.cmp_format({ details = true }),
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'path' },
-                    { name = 'luasnip' },
-                }, {
-                    { name = 'copilot' },
-                    { name = 'buffer' },
-                }),
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end,
-                },
-            })
-            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
-
-            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    { name = 'cmdline' }
-                }),
-                matching = { disallow_symbol_nonprefix_matching = false }
-            })
+            require('bbrain.cmp')
         end,
     },
     {
