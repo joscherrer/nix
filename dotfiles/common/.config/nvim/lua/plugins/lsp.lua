4c10df089ec52c82fbe017062e3ecb89a3e6b456
Jonathan SCHERRER <jonathan.s.scherrer@gmail.com>
2024-07-23 13:14:58 +0200

    fix indent

--- dotfiles/common/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/common/.config/nvim/lua/plugins/lsp.lua
@@ -3,58 +3,48 @@ local function lspconfig_config()
     vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
     vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
-    -- local function format(buffer)
-    --   local filetype = vim.api.nvim_get_option_value('filetype', { buf = buffer })
-    --   if filetype == 'python' then
-    --     vim.cmd('silent! black %')
-    --     return
-    --   end
-    --   vim.lsp.buf.format({ async = true })
-    -- end
-
     local helpers = require('bbrain.helpers')
 
-
     vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function(event)
-        local opts = {buffer = event.buf}
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', function() helpers.format(event.buf) end, opts)
-        vim.keymap.set({'n', 'x'}, 'gq', function() helpers.format(event.buf) end, opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = event.buf })
+        desc = 'LSP actions',
+        callback = function(event)
+            local opts = { buffer = event.buf }
+            vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+            vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+            vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+            vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+            vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+            vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+            vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+            vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+            vim.keymap.set({ 'n', 'x' }, '<F3>', function() helpers.format(event.buf) end, opts)
+            vim.keymap.set({ 'n', 'x' }, 'gq', function() helpers.format(event.buf) end, opts)
+            vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = event.buf })
 
-        vim.api.nvim_create_autocmd("CursorHold", {
-            buffer = event.buf,
-            callback = function()
-                opts = {
-                    focusable = false,
-                    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-                    border = 'rounded',
-                    source = 'always',
-                    prefix = ' ',
-                    scope = 'cursor',
-                }
-                vim.diagnostic.open_float(nil, opts)
-            end
-        })
-      end
+            vim.api.nvim_create_autocmd("CursorHold", {
+                buffer = event.buf,
+                callback = function()
+                    opts = {
+                        focusable = false,
+                        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                        border = 'rounded',
+                        source = 'always',
+                        prefix = ' ',
+                        scope = 'cursor',
+                    }
+                    vim.diagnostic.open_float(nil, opts)
+                end
+            })
+        end
     })
 
     local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     local default_setup = function(server)
-      require('lspconfig')[server].setup({
-        capabilities = lsp_capabilities,
-      })
+        require('lspconfig')[server].setup({
+            capabilities = lsp_capabilities,
+        })
     end
 
     local lsp_server_list = {
