d03ab6a84eb1ec6b379e42cbc8242ff0815c868e
Jonathan SCHERRER <jonathan.s.scherrer@gmail.com>
2024-08-13 22:03:56 +0200

    update

---
+++ hosts/common/default-wsl.nix
@@ -0,0 +1,47 @@
+{ self, inputs, outputs, config, pkgs, lib, ... }:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  security.sudo.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  services.locate.enable = true;
+
+  users.defaultUserShell = pkgs.zsh;
+
+  users.users.jscherrer = {
+    initialHashedPassword = "";
+    isNormalUser = true;
+    group = "jscherrer";
+    extraGroups = [ "wheel" ];
+    createHome = true;
+  };
+
+  users.groups = {
+    better-gc = { };
+    jscherrer = { };
+  };
+
+  security.pam.loginLimits = [
+    { domain = "*"; type = "soft"; item = "nofile"; value = "131070"; }
+  ];
+  systemd.user.extraConfig = "DefaultLimitNOFILE=131070";
+
+  programs.gnupg.agent = {
+    enable = true;
+    # pinentryFlavor = "gnome3";
+    enableSSHSupport = true;
+  };
+}
