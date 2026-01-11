89d0fd9144a2f3b29888d1e62ddd125ecc831fec
Jonathan SCHERRER <jonathan.s.scherrer@gmail.com>
2026-01-11 15:52:51 +0100

    update bbrain-kvm

--- hosts/bbrain-kvm/configuration.nix
+++ hosts/bbrain-kvm/configuration.nix
@@ -82,8 +82,8 @@
       };
     };
     spiceUSBRedirection.enable = true;
-    services.spice-vdagentd.enable = true;
   };
+  services.spice-vdagentd.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
